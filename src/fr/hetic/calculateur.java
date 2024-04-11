3b8a670b12c81fec870111fab0ff4933acc05866
ILYAS EL BAKKALI <ilyaselbakkali2@gmail.com>
2024-04-11 15:00:43 +0200

    dern modif

--- src/fr/hetic/calculateur.java
+++ src/fr/hetic/calculateur.java
@@ -1,32 +1,41 @@
 package fr.hetic;
 
-public class calculateur {
-	public static void main(String[] args) {
-        if (args.length != 1) {
-            System.out.println("Usage: java Calculateur <nomFichier>");
-            return;
-        }
 
-        String fileName = args[0];
-        TraitementFichiers fileProcessor = new TraitementFichiers();
-        fileProcessor.processFile(fileName);
-    }
+	public class calculateur {
+	    public static void main(String[] args) {
+	        if (args.length != 3) {
+	            System.out.println("Usage: java Calculateur <nombre> <nombre> <opérateur>");
+	            return;
+	        }
 
-    public static double calculer(double nombre1, double nombre2, String operateur) {
-        double resultat;
-        switch (operateur) {
-            case "+":
-                resultat = nombre1 + nombre2;
-                break;
-            case "-":
-                resultat = nombre1 - nombre2;
-                break;
-            case "*":
-                resultat = nombre1 * nombre2;
-                break;
-            default:
-                throw new IllegalArgumentException("Opérateur non valide. Utilisez '+', '-' ou '*'.");
-        }
-        return resultat;
-    }
-}
+	        double nombre1, nombre2, resultat;
+	        String operateur;
+	        try {
+	            nombre1 = Double.parseDouble(args[0]);
+	            nombre2 = Double.parseDouble(args[1]);
+	        } catch (NumberFormatException e) {
+	            System.out.println("Les deux premiers paramètres doivent être numériques.");
+	            return;
+	        }
+
+	        operateur = args[2];
+	        switch (operateur) {
+	            case "+":
+	                resultat = nombre1 + nombre2;
+	                System.out.println("Résultat : " + resultat);
+	                break;
+	            case "-":
+	                resultat = nombre1 - nombre2;
+	                System.out.println("Résultat : " + resultat);
+	                break;
+	            case "x":
+	                resultat = nombre1 * nombre2;
+	                System.out.println("Résultat : " + resultat);
+	                break;
+	            default:
+	                System.out.println("Opérateur invalide.");
+	                break;
+	        }
+	    }
+	}
+
