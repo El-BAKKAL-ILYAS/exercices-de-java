600bb87fb1c26de9043c934b9c1bc15158b074f3
ILYAS EL BAKKALI <ilyaselbakkali2@gmail.com>
2024-04-11 15:19:45 +0200

    fixer l'erreur

---
+++ src/fr/hetic/calculator.java
@@ -0,0 +1,32 @@
+package fr.hetic;
+
+public class calculator {
+	  public static void main(String[] args) {
+	        if (args.length != 1) {
+	            System.out.println("Usage: java Calculateur <nomFichier>");
+	            return;
+	        }
+
+	        String fileName = args[0];
+	        FileProcessor fileProcessor = new FileProcessor();
+	        fileProcessor.processFile(fileName);
+	    }
+
+	    public static double calculer(double nombre1, double nombre2, String operateur) {
+	        double resultat;
+	        switch (operateur) {
+	            case "+":
+	                resultat = nombre1 + nombre2;
+	                break;
+	            case "-":
+	                resultat = nombre1 - nombre2;
+	                break;
+	            case "*":
+	                resultat = nombre1 * nombre2;
+	                break;
+	            default:
+	                throw new IllegalArgumentException("Opérateur non valide. Utilisez '+', '-' ou '*'.");
+	        }
+	        return resultat;
+	    }
+}
